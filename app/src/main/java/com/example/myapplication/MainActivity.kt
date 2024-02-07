7deb3c0346d68543836d92b0fa72b2c035b6c36c
yoonjaesung <nury.applogin@gmail.com>
2024-02-07 23:32:40 +0900

    test Commit 3000

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,5 +15,6 @@ class MainActivity : AppCompatActivity() {
         Log.d("test1234", "test4000")
         Log.d("test1234", "test5000")
         Log.d("test1234",  "Test60000")
+        Log.d("test1234", "test700000")
     }
 }
