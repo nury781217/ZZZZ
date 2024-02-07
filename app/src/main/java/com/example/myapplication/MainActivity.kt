49291a9a657425fa3c2f176a17481e26a2eb7bd9
yoonjaesung <nury.applogin@gmail.com>
2024-02-07 23:37:54 +0900

    test Commit 60000

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,12 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("test1234", "aaaa")
-        Log.d("test1234", "test20000")
-        Log.d("test1234", "test3000")
-        Log.d("test1234", "test4000")
-        Log.d("test1234", "test5000")
-        Log.d("test1234",  "Test60000")
-        Log.d("test1234", "test700000")
-        Log.d("test1234", "Test800000")
     }
 }
